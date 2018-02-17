95b45475c4a5a7b754623c94b49c85f5cdd30fa0
LA-angels <34589817+LA-angels@users.noreply.github.com>
2018-02-17 15:31:49 +1100

    Individual website task

---
+++ lessons/task.js
@@ -0,0 +1,9 @@
+var multiply=function(){
+  var a =Math.ceil(Math.random() * 100) ;
+    var b =Math.ceil(Math.random() * 100);
+    console.log(a + " x " + b +" = " + " ? ");
+}
+
+for(var i=0; i<10; i = i + 1) {
+  multiply();
+}
